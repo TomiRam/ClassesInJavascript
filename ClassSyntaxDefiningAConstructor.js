31c8f55d51cc37613a663fcf48cd618113141150
Tomi Rämö <47462643+TomiRam@users.noreply.github.com>
2020-09-08 10:57:55 +0300

    Add files via upload

---
+++ ClassSyntaxDefiningAConstructor.js
@@ -0,0 +1,22 @@
+//use class syntax to define a constructor function
+class SpaceShuttle {
+	constructor (targetPlanet) {
+		this.targetPlanet = targetPlanet;
+	}
+}
+var zeus = new SpaceShuttle('Jupiter');
+
+console.log(zeus.targetPlanet);
+
+function makeClass(){
+	class Vegetable{
+		constructor(name){
+			this.name = name;
+
+		}
+	}
+	return Vegetable;
+}
+const Vegetable = makeClass();
+const carrot = new Vegetable('carrot');
+console.log(carrot.name);
